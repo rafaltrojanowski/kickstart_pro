f35ecb4a7817d2d1e4e9a3f533cf63d58d4e4a3f
Rafał Trojanowski <rt.trojanowski@gmail.com>
2020-12-22 13:40:41 +0100

    Add changeset validation

--- lib/kickstart_web/controllers/subscription_controller.ex
+++ lib/kickstart_web/controllers/subscription_controller.ex
@@ -2,46 +2,65 @@ defmodule KickstartWeb.SubscriptionController do
   use KickstartWeb, :controller
 
   alias Kickstart.Accounts
-  alias Kickstart.Accounts.PricingPlan
 
   alias Gringotts.Gateways.Stripe
   alias Gringotts.CreditCard
 
+  import Ecto.Changeset
+
   def new(conn, %{"pricing_plan_id" => id}) do
+    data  = %{}
+    types = %{first_name: :string, last_name: :string, number: :string, year: :string, month: :string, verification_code: :string}
+
+    changeset =
+      {data, types}
+      |> Ecto.Changeset.cast(data, Map.keys(types))
+      |> validate_required([:first_name, :last_name, :number, :year, :month, :verification_code])
+
     pricing_plan = Accounts.get_pricing_plan!(id)
-    render(conn, "new.html", pricing_plan: pricing_plan)
+    render(conn, "new.html", pricing_plan: pricing_plan, changeset: changeset)
   end
 
   def create(conn, %{"subscription" => subscription_params}) do
-    card = %CreditCard{
-      first_name: subscription_params["first_name"],
-      last_name: subscription_params["last_name"],
-      number: subscription_params["number"],
-      year: subscription_params["year"] |> String.to_integer,
-      month: subscription_params["month"] |> String.to_integer,
-      verification_code:  subscription_params["verification_code"]
-    }
+    pricing_plan = Accounts.get_pricing_plan!(subscription_params["pricing_plan_id"])
 
-    IO.inspect(card)
+    # Validation
+    data  = %{}
+    types = %{first_name: :string, last_name: :string, number: :string, year: :string, month: :string, verification_code: :string}
 
-    # TODO: Take a value from Pricing Plan here:
-    amount = Money.new(19, :USD)
+    changeset =
+      {data, types}
+      |> Ecto.Changeset.cast(subscription_params, Map.keys(types))
+      |> validate_required([:first_name, :last_name, :number, :year, :month, :verification_code])
 
-    IO.inspect(amount)
+    if changeset.valid? do
+      card = %CreditCard{
+        first_name: subscription_params["first_name"],
+        last_name: subscription_params["last_name"],
+        number: subscription_params["number"],
+        year: subscription_params["year"] |> String.to_integer,
+        month: subscription_params["month"] |> String.to_integer,
+        verification_code:  subscription_params["verification_code"]
+      }
+      IO.inspect(card)
+      # TODO: Take a value from Pricing Plan here:
+      amount = Money.new(19, :USD)
+      IO.inspect(amount)
 
-    value = Gringotts.purchase(Stripe, amount, card)
+      value = Gringotts.purchase(Stripe, amount, card)
 
-    cond do
-      value["created"] ->
-        IO.inspect("SUCCESS")
-        IO.inspect(value)
-        render(conn, "success.html")
-      value["error"] ->
-        IO.inspect("ERROR")
-        IO.inspect(value)
-
-        render(conn, "error.html")
+      cond do
+        value["created"] ->
+          IO.inspect("SUCCESS")
+          IO.inspect(value)
+          render(conn, "success.html")
+        value["error"] ->
+          IO.inspect("ERROR")
+          IO.inspect(value)
+          render(conn, "error.html")
+      end
+    else
+      render(conn, "new.html", pricing_plan: pricing_plan, changeset: %{changeset | action: :insert})
     end
-
   end
 end
