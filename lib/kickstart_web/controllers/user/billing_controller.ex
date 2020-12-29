9c9c1bc0e2d64b474dc62a274d9c55db308f8b59
Rafał Trojanowski <rt.trojanowski@gmail.com>
2020-12-29 12:09:32 +0100

    [Fix] Billing page
    - Handle the case if there is no existing subscription

--- lib/kickstart_web/controllers/user/billing_controller.ex
+++ lib/kickstart_web/controllers/user/billing_controller.ex
@@ -13,10 +13,18 @@ defmodule KickstartWeb.BillingController do
       where: s.user_id == ^user_id and s.start_at < ^time_now and (s.end_at > ^time_now or is_nil(s.end_at)),
       select: [s.id, s.pricing_plan_id]
 
-    current_subscription_id = Enum.at(Repo.one(query), 0)
-    current_pricing_plan_id = Enum.at(Repo.one(query), 1)
-    current_pricing_plan = Accounts.get_pricing_plan!(current_pricing_plan_id)
-    current_subscription = Accounts.get_subscription!(current_subscription_id)
+    current_subscription = Repo.one(query)
+
+    {current_pricing_plan, current_subscription} = if current_subscription do
+      current_subscription_id = Enum.at(current_subscription, 0)
+      current_pricing_plan_id = Enum.at(current_subscription, 1)
+      current_pricing_plan = Accounts.get_pricing_plan!(current_pricing_plan_id)
+      current_subscription = Accounts.get_subscription!(current_subscription_id)
+
+      {current_pricing_plan, current_subscription}
+    else
+      {nil, nil}
+    end
 
     render(conn, "index.html", current_pricing_plan: current_pricing_plan, current_subscription: current_subscription)
   end
